ee96c0a352e7e3070659b996e1011afd9df1a02e
Byron Gibson <fbg111@gmail.com>
2012-07-23 20:43:19 -0700

    update

--- lib/scripts/cp-ant-properties.sh
+++ lib/scripts/cp-ant-properties.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash -
 
+#Copy ant-build-script properties files, and convert to SBT properties files by 
+#replacing = with :=
+
 cp ./lib/submodules/ant-build-script/config/default.properties ./project/default.properties.ant
 cp ./lib/submodules/ant-build-script/config/project.properties ./project/project.properties.ant
+
 
